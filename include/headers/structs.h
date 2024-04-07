92d33ba938e24478a14c214a41dd9e5b5f3923a4
Mirza Adnan <mirza.adnan2205@gmail.com>
2024-04-08 01:17:37 +0600

    added a cat struct

--- include/headers/structs.h
+++ include/headers/structs.h
@@ -1,6 +1,25 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
 #include "common.h"
 
 typedef struct {
   SDL_Window* window;
   SDL_Renderer* renderer;
 } Graphics;
+
+typedef struct {} Game;
+
+typedef struct {
+  float x, y;
+  float dx, dy;
+  int w,h;
+  short lives;
+  bool facingRight;
+  bool slowingDown;
+  bool onPlatform;
+  bool hasJumped;
+  bool continueJump;
+} Cat;
+
+#endif
